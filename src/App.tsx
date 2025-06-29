c5e8dac8f2964ce4847df549f6f5a03966772808
Kate Martin <katejaneen23@gmail.com>
2025-06-29 22:17:04 +0800

    Issue #3 WIP - Resolve Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import logo from './assets/logo-v1.png';
 function App() {
   return (
     <div className="w-full toothline-secondary p-5">
       {/* Header */}
+      <header className="max-w-6xl flex flex-wrap mx-auto">
+        <div className="w-1/2">
+          <img src={logo} className='w-40' />
+        </div>
+        <div className="w-1/2 text-right my-auto">
+          <button type="button" className="font-opensans font-bold toothline-accent hover:toothline-accent-hover text-white px-3 py-2 rounded-md text-sm transition ease-in-out duration-300">Book Appointment</button>
+        </div>
+      </header>
       {/* Main */}
       {/* Footer */}
     </div>
