04016f6c9dbde11fc26ceadb68c38a3885487ed8
Kate Janeen Martin <53368594+katemartiiin@users.noreply.github.com>
2025-07-04 01:59:25 +0800

    Issue #7 - Resolve Admin FE Login page (#27)

--- src/pages/Admin/LoginPage.tsx
+++ src/pages/Admin/LoginPage.tsx
@@ -1,9 +1,51 @@
+import logo from '../../assets/logo-admin-white.png';
 const LoginPage: React.FC = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center text-center">
-      <h1 className="text-2xl font-semibold">
-        Login Page Coming Soon 🚧
-      </h1>
+    <div className="min-h-screen flex items-center justify-center toothline-primary">
+      <div className="toothline-primary-login p-8 rounded-lg shadow-md w-full max-w-sm">
+        <div className="flex items-center justify-center">
+          <img
+            src={logo}
+            alt="Toothline Logo"
+          />
+        </div>
+
+        <form className="font-opensans">
+          <div className="mb-4">
+            <label className="block text-white mb-1 text-sm" htmlFor="email">
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              className="w-full px-3 py-2 rounded bg-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-teal-300 text-sm"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-white mb-1 text-sm" htmlFor="password">
+              Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              className="w-full px-3 py-2 rounded bg-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-teal-300 text-sm"
+            />
+            <div className="text-right mt-1">
+              <a href="#" className="text-xs text-white hover:underline">
+                Forgot your password?
+              </a>
+            </div>
+          </div>
+
+          <button
+            type="submit"
+            className="text-sm w-full toothline-primary-darker hover:bg-teal-800 text-white font-semibold py-2 px-4 rounded mt-4"
+          >
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
